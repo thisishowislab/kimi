c6d95c5854c0ed4eeae3280d5acd66d7a87499c9
thisishowislab <thisishowislab@gmail.com>
2026-02-15 14:14:39 -0800

    Fix donation Stripe price ID mapping and checkout guardrails

--- build.js
+++ build.js
@@ -1,28 +1,12 @@
-const fs = require('fs');
-const path = require('path');
+import { buildContentData } from './scripts/contentful-sync.js';
 
-const spaceId = process.env.CONTENTFUL_SPACE_ID;
-const token = process.env.CONTENTFUL_TOKEN;
-
-if (!spaceId || !token) {
-    console.error('Missing CONTENTFUL_SPACE_ID or CONTENTFUL_TOKEN');
+buildContentData()
+  .then((data) => {
+    console.log(
+      `Built data files: ${data.products.length} products, ${data.tours.length} tours, ${data.donations.length} donations, ${data.posts.length} posts.`
+    );
+  })
+  .catch((error) => {
+    console.error(error);
     process.exit(1);
-}
-
-// Find all HTML files
-const files = fs.readdirSync('.').filter(f => f.endsWith('.html'));
-
-files.forEach(file => {
-    let html = fs.readFileSync(file, 'utf8');
-    
-    // Only process if it has placeholders
-    if (html.includes('{{CONTENTFUL_SPACE_ID}}')) {
-        html = html
-            .replace(/{{CONTENTFUL_SPACE_ID}}/g, spaceId)
-            .replace(/{{CONTENTFUL_TOKEN}}/g, token);
-        
-        fs.writeFileSync(file, html);
-        console.log(`Injected credentials into ${file}`);
-    }
-});
-
+  });
